cb1181fb694450283c4545f36219be09ffeec1a4
Eiko <eiko.baeumker@imtek.de>
2019-09-22 14:19:21 +0200

    Aded check if conversion is successful

--- script/check.sh
+++ script/check.sh
@@ -6,6 +6,6 @@ inotifywait -m $input -e create -e moved_to |
     while read path action file; do
         echo "The file '$file' appeared in directory '$path' via '$action'"
 	sleep 2
-	echo "convert '$input/$file' '$output/$file.pdf'" 
-	convert "$input/$file" "$output/$file.pdf" 
+	echo "convert '$input/$file' '$output/$file.pdf'"
+	convert "$input/$file" "$output/$file.pdf" || (sleep 30 && convert "$input/$file" "$output/$file.pdf")
     done
