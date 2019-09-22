425794141211a069afb8e26ab1d69d3936ad1afd
Eiko <eiko.baeumker@imtek.de>
2019-09-22 14:31:51 +0200

    Added redoing in failure

--- script/check.sh
+++ script/check.sh
@@ -7,5 +7,5 @@ inotifywait -m $input -e create -e moved_to |
         echo "The file '$file' appeared in directory '$path' via '$action'"
 	sleep 2
 	echo "convert '$input/$file' '$output/$file.pdf'"
-	convert "$input/$file" "$output/$file.pdf" || (sleep 30 && convert "$input/$file" "$output/$file.pdf")
+	convert "$input/$file" "$output/$file.pdf" || ( echo "Failed - wait 30s" && sleep 30 && convert "$input/$file" "$output/$file.pdf") ||  ( echo "Failed - wait 30s" && sleep 30 && convert "$input/$file" "$output/$file.pdf")
     done
